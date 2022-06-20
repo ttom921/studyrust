d778377a217a6d65631b1a37315f9a4efe15560d
ttom921 <ttom4029>
2022-06-20 21:45:54 +0800

    第二個

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io; //trait
 fn main() {
     println!("猜數!");
@@ -9,5 +10,15 @@ fn main() {
     println!("猜測一個數");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("無法讀取行");
+
+    // shodow
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
     println!("你猜測的數是: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"), //arm
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
