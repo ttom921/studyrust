bf749d86a8b1122523a0974ba3155ea40b9ea3bd
ttom921 <ttom921@hotmail.com>
2024-05-02 09:57:33 +0800

    fest: rust 測試_Test

--- base_grammar/為你自己學Rust_高見龍/base01/src/main.rs
+++ base_grammar/為你自己學Rust_高見龍/base01/src/main.rs
@@ -830,16 +830,44 @@
 // region: 屬性（Attributes）
 
 // #[allow(dead_code)]
-#[derive(Debug)]
-struct Cat {
-    name: String,
-    age: u8,
-}
-fn main() {
-    let kitty = Cat {
-        name: String::from("kitty"),
-        age: 18,
-    };
-    println!("{:?}", kitty);
-}
+// #[derive(Debug)]
+// struct Cat {
+//     name: String,
+//     age: u8,
+// }
+// fn main() {
+//     let kitty = Cat {
+//         name: String::from("kitty"),
+//         age: 18,
+//     };
+//     println!("{:?}", kitty);
+// }
 // endregion: 屬性（Attributes）
+// region: 測試（Test）
+
+#[cfg(test)]
+mod bmi {
+    use crate::bmi_calc;
+    #[test]
+    fn dummy() {
+        let result = 1 + 2;
+        assert_eq!(result, 3);
+    }
+    #[test]
+    fn test_calc() {
+        let result = bmi_calc(180, 65);
+        assert_eq!(result, 20.1);
+    }
+}
+fn bmi_calc<T, U>(height: T, weight: U) -> f64
+where
+    T: Into<f64>,
+    U: Into<f64>,
+{
+    let h = height.into() / 100.0;
+    let bmi = weight.into() / (h * h);
+    (bmi * 10.0).round() / 10.0
+}
+
+fn main() {}
+// endregion: 測試（Test）
